b1a97c161d198bd2708f0d6cdfe12f9185a66284
Anis <admin@Macbook-Pro-de-Anis.local>
2021-01-28 17:15:54 +0100

    deployment on heroku ok

--- 2_Deployment/getprev.py
+++ 2_Deployment/getprev.py
@@ -1,9 +1,6 @@
 import pandas as pd
-from flask import Flask, url_for, request, jsonify, json
 from joblib import dump, load
-from werkzeug.exceptions import HTTPException
 import re
-import spacy
 from spacy.lang.fr.stop_words import STOP_WORDS
 import fr_core_news_sm
 nlp = fr_core_news_sm.load()
@@ -41,11 +38,14 @@ def get_prev(text):
 
     #Categorizing meta values, the bins are the quantiles used to cut the original dataset (see meta_data_analysis notebook)
     #length
-    meta['len_review'] = pd.cut(meta['len_review'], bins=[   5.,  368., 4998.], labels = ['low', 'high'], include_lowest=True)
-    #uppercase
-    meta['upper_word_count'] = pd.cut(meta['upper_word_count'], bins=[  0.,   1,   2, 147.], labels = ['low', 'mid', 'high'],           include_lowest=True)
+    meta.loc[:, 'len_review'] = pd.cut(meta.loc[:, 'len_review'], bins=[5.,  368., 4998.], labels = ['low', 'high'], include_lowest=True)
+    
     #exclam
-    meta['exclam_count'] = pd.cut(meta['exclam_count'], bins=[  0.,   1,   3., 133.],labels=['low', 'high', 'very_high'], include_lowest=True)
+    meta.loc[:, 'exclam_count'] = pd.cut(meta.loc[:, 'exclam_count'], bins=[0.,   1,   3., 133.],labels=['low', 'high', 'very_high'], include_lowest=True)
+    
+    #uppercase
+    meta.loc[:, 'upper_word_count'] = pd.cut(meta.loc[:, 'upper_word_count'], bins=[0.,   1,   2, 147.], labels = ['low', 'mid', 'high'], include_lowest=True)
+    
 
     #We concatenate the topic and the metadata 
     conc = pd.concat([topic, meta], axis = 1)
@@ -55,4 +55,4 @@ def get_prev(text):
     prev = lr.predict(data)
     proba = lr.predict_proba(data)
 
-    return prev[0], proba[0][0], proba[0][1], conc[['len_review','exclam_count','upper_word_count']]
+    return prev[0], proba[0][0], proba[0][1],
--- 2_Deployment/test.py
+++ 2_Deployment/test.py
@@ -1,9 +1,9 @@
 import requests
 
-ENDPOINT = "http://127.0.0.1:5000/predict"
+#ENDPOINT = "http://127.0.0.1:5000/predict"
 
-test_ok = requests.post(ENDPOINT, json={"review": "ceci est un test voyons voir si ca marche comme VRAIMENT PAS BON !!!!! ca "})
-print(test_ok.text)
+#test_ok = requests.post(ENDPOINT, json={"review": "ceci est un test voyons voir si ca marche comme VRAIMENT PAS BON !!!!! ca "})
+#print(test_ok.text)
 
 #test_ok_rounded = requests.post(ENDPOINT, json={"year": 5, "rounded": "True"})
 #print(test_ok_rounded, test_ok_rounded.json())
