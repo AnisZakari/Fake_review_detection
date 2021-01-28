457e3dfc2b368daf6ec4ea6f1d1b4414aa3d32b7
Anis <admin@Macbook-Pro-de-Anis.local>
2021-01-28 01:16:06 +0100

    update of the .py files

--- 2_Deployment/app.py
+++ 2_Deployment/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+import pandas as pd
+import numpy as np
+from flask import Flask, url_for, request, jsonify, json
+from joblib import dump, load
+from werkzeug.exceptions import HTTPException
+from flask import Flask, url_for, request, jsonify, json
+from getprev import get_prev
 
 app = Flask(__name__)
 
 
-@app.route("/)
+@app.route("/")
 def index():
     return render_template("index.html")
 
@@ -15,6 +22,30 @@ def more_love():
         "Cookies",
     ]
     return render_template("more_love.html", things_i_love=things_i_love)
+
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Check parameters
+    if request.json:
+        # Get JSON as dictionnary
+        json_input = request.get_json()
+        value = [[json_input['review']]]
+        label, proba_real, proba_fake, text_infos = get_prev(str(value))
+        if label == 0:
+            r = "Cette review à l'air légitime, avec une probabilité de " + str(round(proba_real,3))
+        if label == 1:
+            r = "Cette review est probablement fausse avec une probabilité de " + str(round(proba_fake,3))
+
+        # Return prediction
+        #response = {
+            # Since prediction is a float and jsonify function can't handle
+            # floats we need to convert it to string
+        #    "prediction": str(r),
+        #}
+        
+        return r
 
 
 if __name__ == "__main__":
--- 2_Deployment/getprev.py
+++ 2_Deployment/getprev.py
@@ -1,3 +1,22 @@
+import pandas as pd
+from flask import Flask, url_for, request, jsonify, json
+from joblib import dump, load
+from werkzeug.exceptions import HTTPException
+import re
+import spacy
+from spacy.lang.fr.stop_words import STOP_WORDS
+import fr_core_news_sm
+nlp = fr_core_news_sm.load()
+
+
+# Load Tools to preprocess and extract topics
+lr = load('model_files/main_model.pkl')
+preprocessor = load('model_files/preprocessor.pkl')
+text_vectorizer = load('model_files/text_vectorizer.pkl')
+topic_extractor = load('model_files/topic_extractor.pkl')
+
+
+
 def get_prev(text):
     #Cleaning
     review = text.replace('\n', ' ')
@@ -6,7 +25,7 @@ def get_prev(text):
     exclam_count = len(''.join(ch for ch in review if ch =='!'))
     uppercase_word_count = sum(map(str.isupper, review.split()))
 
-    meta = pd.DataFrame([[length, uppercase_word_count, exclam_count]], columns = ['len_review', 'exclam_count', 'upper_word_count'])
+    meta = pd.DataFrame([[length, exclam_count, uppercase_word_count]], columns = ['len_review', 'exclam_count', 'upper_word_count'])
     #Cleaning, Vectorization and Topic Extraction
 
     review_clean = re.sub(r"[^A-zÀ-ÿ0-9' ]+", " ", review).lower()
@@ -24,9 +43,9 @@ def get_prev(text):
     #length
     meta['len_review'] = pd.cut(meta['len_review'], bins=[   5.,  368., 4998.], labels = ['low', 'high'], include_lowest=True)
     #uppercase
-    meta['upper_word_count'] = pd.cut(meta['upper_word_count'].rank(method = 'first'), bins=[  0.,   3,   6, 147.], labels = ['low', 'mid',    'high'], include_lowest=True)
+    meta['upper_word_count'] = pd.cut(meta['upper_word_count'], bins=[  0.,   1,   2, 147.], labels = ['low', 'mid', 'high'],           include_lowest=True)
     #exclam
-    meta['exclam_count'] = pd.cut(meta['exclam_count'].rank(method = 'first'), bins=[  0.,   0.01,   2., 133.],labels=['low', 'high',          'very_high'], include_lowest=True)
+    meta['exclam_count'] = pd.cut(meta['exclam_count'], bins=[  0.,   1,   3., 133.],labels=['low', 'high', 'very_high'], include_lowest=True)
 
     #We concatenate the topic and the metadata 
     conc = pd.concat([topic, meta], axis = 1)
@@ -34,5 +53,6 @@ def get_prev(text):
     #preprocessing 
     data = preprocessor.transform(conc)
     prev = lr.predict(data)
+    proba = lr.predict_proba(data)
 
-    return prev[0]
+    return prev[0], proba[0][0], proba[0][1], conc[['len_review','exclam_count','upper_word_count']]
