119af40110ae864b3050b3279596390b19a8b79b
Anis <admin@Macbook-Pro-de-Anis.local>
2021-03-01 17:43:30 +0100

    update

--- 2_Deployment/app.py
+++ 2_Deployment/app.py
@@ -3,60 +3,68 @@ import pandas as pd
 import numpy as np
 from joblib import dump, load
 from werkzeug.exceptions import HTTPException
-from getprev import get_prev
+from getprev import get_prev_full
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+    
+@app.route("/", methods=['POST'])
+def get_prediction():
+
+    review = request.form['review']
+    len_value = len("".join(ch for ch in review))
+    photos_for_review = request.form['photos_for_review']
+    rating = request.form['rating']
+    user_friends_count = request.form['user_friends_count']
+    user_reviews_count = request.form['user_reviews_count']
+    restaurant_average_rating = request.form['restaurant_average_rating']
+    restaurant_reviews_count = request.form['restaurant_reviews_count']
+
+    if photos_for_review == 'yes':
+        photos_for_review = 2
+    if photos_for_review == 'no':
+        photos_for_review = 0
+
+    #test = str(photos_for_review) + ' ' + str(rating) + ' ' + str(user_friends_count) + ' ' + str(user_reviews_count) + ' ' + str(restaurant_average_rating) + ' ' + str(restaurant_reviews_count)
+
+    all_values_ok = (photos_for_review != '') & (rating != '' ) & (rating != '' ) & (user_friends_count != '' ) & (user_reviews_count != '' ) & (restaurant_average_rating != '' ) & (restaurant_reviews_count != '' )
+
+    len_review = len(''.join(ch for ch in str(review) if ch.isalnum()))
+
+    if len_value <= 10:
+
+        r = "Veuillez écrire une review plus longue."
+        label ='empty'
+
+    
+    if (all_values_ok == False) :
+        r = "Veuillez renseigner toutes les informations."
+        label ='empty'
+    
+
+
+    if (all_values_ok) & (len_value >10):
+        label, proba_fake = get_prev_full(review, float(photos_for_review), float(rating), float(user_friends_count), float(user_reviews_count), float(restaurant_average_rating), float(restaurant_reviews_count), threshold = 0.5)
+        
+            label = 0
+        
+        if label == 1:
+            r = "Cette review est probablement fausse " # + "avec une probabilité de " + str(round(proba_fake,3))
+        if label == 0:          
+            r = "Cette review à l'air légitime "  #+ "avec une probabilité de " + str(round(1-proba_fake,3))
+    return render_template("send_prediction.html", label = label, review = review, response = r)
 
 @app.route("/dashboard")
 def dash():
     return render_template("dashboard.html")
-
-@app.route("/", methods=['POST'])
-def my_form_post():
-    value = request.form['text']
-    len_value = len(''.join(ch for ch in str(value) if ch.isalnum()))
-
-    if len_value < 10:
-
-        r = "Veuillez écrire une review plus longue."
-    
-    else:
-        label, proba_real, proba_fake = get_prev(str(value))
-        if label == 0:
-            r = "Cette review à l'air légitime, avec une probabilité de " + str(round(proba_real,3))
-        if label == 1:
-            r = "Cette review est probablement fausse avec une probabilité de " + str(round(proba_fake,3))
-
-    return r
-
-
-
-
-
-'''
-@app.route('/backup', methods=['POST'])
-def my_form_post():
-    value = request.form['text']
-    len_value = len(''.join(ch for ch in str(value) if ch.isalnum()))
-
-    if len_value < 10:
-
-        r = "Veuillez écrire une review plus longue."
-    
-    else:
-        label, proba_real, proba_fake, text_infos = get_prev(str(value))
-        if label == 0:
-            r = "Cette review à l'air légitime, avec une probabilité de " + str(round(proba_real,3))
-        if label == 1:
-            r = "Cette review est probablement fausse avec une probabilité de " + str(round(proba_fake,3))
-
-    return r
-'''
 
 if __name__ == "__main__":
     app.run(debug=True)
--- 2_Deployment/getprev.py
+++ 2_Deployment/getprev.py
@@ -4,15 +4,58 @@ import re
 from spacy.lang.fr.stop_words import STOP_WORDS
 import fr_core_news_sm
 nlp = fr_core_news_sm.load()
+import tensorflow as tf
+from keras.models import load_model
+import numpy as np
+import keras
+
+@tf.function
+def macro_soft_f1(y, y_hat):
+    """Compute the macro soft F1-score as a cost (average 1 - soft-F1 across all labels).
+    Use probability values instead of binary predictions.
+    
+    Args:
+        y (int32 Tensor): targets array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
+        
+    Returns:
+        cost (scalar Tensor): value of the cost function for the batch
+    """
+    y = tf.cast(y, tf.float32)
+    y_hat = tf.cast(y_hat, tf.float32)
+    tp = tf.reduce_sum(y_hat * y, axis=0)
+    fp = tf.reduce_sum(y_hat * (1 - y), axis=0)
+    fn = tf.reduce_sum((1 - y_hat) * y, axis=0)
+    soft_f1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    cost = 1 - soft_f1 # reduce 1 - soft-f1 in order to increase soft-f1
+    macro_cost = tf.reduce_mean(cost) # average on all labels
+    return macro_cost
+
+keras.losses.custom_loss = macro_soft_f1
 
 
-# Load Tools to preprocess and extract topics
+# Load Tools to preprocess and extract topics for function get_prev
+#---------------------------------------#
 lr = load('model_files/main_model.pkl')
 preprocessor = load('model_files/preprocessor.pkl')
 text_vectorizer = load('model_files/text_vectorizer.pkl')
 topic_extractor = load('model_files/topic_extractor.pkl')
+#---------------------------------------#
 
 
+# Load Tools to preprocess and extract topics for function get_prev_full
+#---------------------------------------#
+model = tf.keras.models.load_model('model_files/tensorflow/model', custom_objects={'macro_soft_f1': macro_soft_f1})
+preprocessor = load('model_files/tensorflow/preprocessor.pkl')
+text_vectorizer = load('model_files/tensorflow/vectorizer.pkl')
+topic_extractor = load('model_files/tensorflow/topic_extract.pkl')
+
+superlatifs = ['bon', 'bonne', 'bons', 'bonnes', 'meilleur', 'meilleure', 'meilleures',
+'mauvais', 'mauvaise', 'pire','petit', 'petite', 'moindre','plus', 'mieux', 'gros',
+'impossible', 'totalement', 'loin', 'absence', 'très', 'tres', 'décu', 'decu',
+'trop', 'jamais', 'toujours', 'aucun', 'déplorable', 'éviter', 'eviter', 'absolument',
+'infect', 'infecte', 'fuir', 'fuire']
+#---------------------------------------#
 
 def get_prev(text):
     #Cleaning
@@ -56,3 +99,88 @@ def get_prev(text):
     proba = lr.predict_proba(data)
 
     return prev[0], proba[0][0], proba[0][1],
+
+
+
+
+preprocessor = load('model_files/tensorflow/preprocessor.pkl')
+text_vectorizer = load('model_files/tensorflow/vectorizer.pkl')
+topic_extractor = load('model_files/tensorflow/topic_extract.pkl')
+
+superlatifs = ['bon', 'bonne', 'bons', 'bonnes', 'meilleur', 'meilleure', 'meilleures',
+'mauvais', 'mauvaise', 'pire','petit', 'petite', 'moindre','plus', 'mieux', 'gros',
+'impossible', 'totalement', 'loin', 'absence', 'très', 'tres', 'décu', 'decu',
+'trop', 'jamais', 'toujours', 'aucun', 'déplorable', 'éviter', 'eviter', 'absolument',
+'infect', 'infecte', 'fuir', 'fuire']
+
+def get_prev_full(text, photos_for_review, rating, user_friends_count,
+user_reviews_count, restaurant_average_rating, restaurant_reviews_count,
+threshold = 0.5):
+
+    #input data
+    photos_for_review = photos_for_review
+    rating = rating
+    user_friends_count = user_friends_count
+    user_reviews_count = user_reviews_count
+    restaurant_average_rating = restaurant_average_rating
+    restaurant_reviews_count = restaurant_reviews_count
+
+    #Cleaning text
+    review = text.replace('\n', ' ')
+    review_clean = re.sub(r"[^A-zÀ-ÿ0-9' ]+", " ", review).lower()
+    review_clean = re.sub(' +', ' ',review_clean).strip()
+    lemmatized = " ".join([token.lemma_ for token in nlp(review_clean)])
+
+    #Feature Engineering
+    text_length = len(review)
+    exclam_count = len(''.join(ch for ch in review if ch =='!'))
+    upper_word_count = sum(map(str.isupper, review.split()))
+    punctuation = len(''.join(ch for ch in review if ch ==',' or ch =='.' or ch ==';' or ch ==':' or ch =='?' or ch.isnumeric() ))
+    sup_count = len([word for word in review_clean.split() if word in superlatifs])
+    euros = len(''.join(ch for ch in review if ch =='€'))
+    if euros >0:
+        euros = 1
+
+    negation = len([word for word in review_clean.split() if word in ['n', 'ne']])
+    isnum = len(''.join(ch for ch in review_clean if ch.isnumeric()) )
+    if isnum >0:
+        isnum = 1
+
+
+
+    #Vectorization and Topic Extraction
+    vectrorized_item = text_vectorizer.transform([lemmatized])
+    topic = topic_extractor.transform(vectrorized_item)
+
+    meta = np.array([[
+            photos_for_review,
+            rating, 
+            user_friends_count,
+            user_reviews_count,
+            restaurant_average_rating,
+            restaurant_reviews_count,
+            text_length,
+            exclam_count, 
+            upper_word_count,
+            punctuation,
+            sup_count,
+            euros,
+            negation,
+            isnum 
+                ]])        
+
+    #preprocessing metadata
+    metadata = preprocessor.transform(meta)
+     
+
+    #We concatenate the topic and the metadata 
+    conc = np.concatenate([topic, metadata], axis = 1)
+
+    #prediction
+    proba_of_fake = model.predict(conc)
+    if proba_of_fake > threshold:
+        prediction = 1
+    else:
+        prediction = 0
+
+    return prediction, proba_of_fake[0][0]
